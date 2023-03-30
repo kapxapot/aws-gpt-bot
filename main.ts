c2a396704a2aeaef9d1bac2b69945d5f7224003a
Sergey Atroshchenko <kapxapot@gmail.com>
2023-03-30 11:09:27 +0400

    Updates bot setup

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ import { userName } from "./src/lib/common";
 const token = process.env.BOT_TOKEN!;
 const bot = new Telegraf(token);
 
-bot.command("start", ctx => {
+bot.start(ctx => {
   console.log(ctx.from);
   ctx.reply(`Добро пожаловать, ${userName(ctx)}! Здесь можно пообщаться с ИИ GPT-3.`);
 })
@@ -15,7 +15,11 @@ bot.on("text", async ctx => {
   const text = ctx.message.text;
   const gptReply = await chatCompletion(text);
 
-  ctx.reply(gptReply ?? "Нет ответа от OpenAI API. :(")
+  ctx.reply(gptReply ?? "Нет ответа от OpenAI API. :(");
+});
+
+bot.catch((err, ctx) => {
+  console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
 });
 
 export const gpt = http(bot.webhookCallback("/telegraf"));
